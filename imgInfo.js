6f422504d759f2a3fe3eb3d767d6256c96b28de4
Justin Braaten <jstnbraaten@gmail.com>
2016-08-01 14:19:52 -0700

    changing example images

--- imgInfo.js
+++ imgInfo.js
@@ -1,1 +1,19 @@
-info = {"refInfo":{"origFile":"J:/l1g_warp/test/refimg/LM50450301985132AAA04/LM50450301985132_archv.tif","imgFile":"LM50450301985132_archv.png","origRows":4002,"origCols":4216,"imgRows":4002,"imgCols":4216},"fixInfo":{"origFile":["J:/l1g_warp/test/fiximg/LM40450301982276AAA03/LM40450301982276_archv.tif","J:/l1g_warp/test/fiximg/LM40450301982292AAA01/LM40450301982292_archv.tif","J:/l1g_warp/test/fiximg/LM40450301982356AAA04/LM40450301982356_archv.tif","J:/l1g_warp/test/fiximg/LM40450301983039XXX03/LM40450301983039_archv.tif","J:/l1g_warp/test/fiximg/LM40450301983055AAA03/LM40450301983055_archv.tif"],"imgFile":["LM40450301982276_archv.png","LM40450301982292_archv.png","LM40450301982356_archv.png","LM40450301983039_archv.png","LM40450301983055_archv.png"],"origRows":[3990,3975,3999,4001,3992],"origCols":[4199,4190,4204,4207,4200],"imgRows":[3990,3975,3999,4001,3992],"imgCols":[4199,4190,4204,4207,4200]}}
+info={
+	"refInfo": {
+		"origFile": "J:/lcms_l1g2l1t/refimgs/LM50160371992077_archv.tif",
+		"imgFile": "LM50160371992077_archv.png",
+		"origRows": 3013,
+		"origCols": 3239,
+		"imgRows": 3013,
+		"imgCols": 3239
+	},
+	"fixInfo": {
+		"origFile": ["J:/lcms_l1g2l1t/fiximgs/1637/wrs2/1637/targz/LM40160371983124AAA03/LM40160371983124_archv.tif",
+		"J:/lcms_l1g2l1t/fiximgs/1637/wrs2/1637/targz/LM50160371984167AAA03/LM50160371984167_archv.tif"],
+		"imgFile": ["LM40160371983124_archv.png","LM50160371984167_archv.png"],
+		"origRows": [2991,3039],
+		"origCols": [3226,3233],
+		"imgRows": [2991,3039],
+		"imgCols": [3226,3233]
+	}
+}
